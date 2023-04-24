8fb2cdef05f0ae89c727c04470e3f05cbbb9434b
arthurchina92 <arthurchinaa@gmail.com>
2023-04-24 16:57:08 -0600

    First configuration of the app, added flatlist component to render coinItem

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import { CoinItem } from "./CoinItem";
+
+function App() {
+  const [coins,setCoins] = useState([])
+  const loadData = async () => {
+    const res = await fetch(
+      'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en'
+      );
+      const data = await res.json()
+      console.log(data);
+      setCoins(data)
+  } 
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+         data={coins}
+         renderItem={({item}) => {
+          return <CoinItem coin={item}/>
+         }}
+      
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#141414",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export {App};
---
+++ src/CoinItem.js
@@ -0,0 +1,35 @@
+import { StyleSheet, Text, View, Image } from "react-native";
+import React from "react";
+
+function CoinItem({ coin }) {
+  return (
+    <View style={styles.itemContainer}>
+      <View style={styles.coinName}>
+        <Image style={styles.coinImage} source={{ uri: coin.image }} />
+        <Text style={styles.coinText}>{coin.name}</Text>
+      </View>
+      <Text style={styles.coinText}>$100000</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  itemContainer: {
+    backgroundColor: "#d0d0",
+    paddingTop: 20,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  coinText: {
+    color: "#ffff",
+  },
+  coinImage: {
+    height: 30,
+    width: 30,
+  },
+  coinName: {
+    flexDirection: "row",
+  },
+});
+
+export { CoinItem };
