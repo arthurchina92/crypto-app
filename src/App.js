c157dce38a9bfc50ab977785090394fba81989b7
arthurchina92 <arthurchinaa@gmail.com>
2023-04-27 16:58:25 -0600

    Added information about the coins: symbol, value, price change, organised styles in App.js and CoinItem.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useEffect, useState } from "react";
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, TextInput, View } from "react-native";
 import { CoinItem } from "./CoinItem";
 
 function App() {
-  const [coins,setCoins] = useState([])
+  const [coins, setCoins] = useState([]);
   const loadData = async () => {
     const res = await fetch(
-      'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en'
-      );
-      const data = await res.json()
-      console.log(data);
-      setCoins(data)
-  } 
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en"
+    );
+    const data = await res.json();
+    console.log(data);
+    setCoins(data);
+  };
   useEffect(() => {
     loadData();
   }, []);
 
   return (
     <View style={styles.container}>
+      <StatusBar backgroundColor="#fff" />
+      <View style={styles.header}>
+        <Text style={styles.title}>Crypto Market</Text>
+        <TextInput style={styles.searchInput} />
+      </View>
       <FlatList
-         data={coins}
-         renderItem={({item}) => {
-          return <CoinItem coin={item}/>
-         }}
-      
+        style={styles.list}
+        data={coins}
+        renderItem={({ item }) => {
+          return <CoinItem coin={item} />;
+        }}
+        showsVerticalScrollIndicator={false}
       />
       <StatusBar style="auto" />
     </View>
@@ -34,10 +40,34 @@ function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#1414144",
+    backgroundColor: "#141414",
     alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    color: "#fff",
+    fontSize: 25,
+    marginTop: 30,
+  },
+  list: {
+    width: "85%",
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "90%",
+    marginBottom: 15,
+  },
+  searchInput: {
+    marginTop: 35,
+    color: "#fff",
+    borderBottomColor: "#4657CE",
+    borderBottomWidth: 1,
+    width: "40%",
+    height: "40%",
+    textAlign: "center",
     justifyContent: "center",
   },
 });
 
-export {App};
+export { App };
--- src/CoinItem.js
+++ src/CoinItem.js
@@ -6,9 +6,24 @@ function CoinItem({ coin }) {
     <View style={styles.itemContainer}>
       <View style={styles.coinName}>
         <Image style={styles.coinImage} source={{ uri: coin.image }} />
-        <Text style={styles.coinText}>{coin.name}</Text>
+        <View style={styles.textNameSymbolContainer}>
+          <Text style={styles.coinText}>{coin.name}</Text>
+          <Text style={styles.symbolText}>{coin.symbol}</Text>
+        </View>
       </View>
-      <Text style={styles.coinText}>$1000000</Text>
+      <View>
+        <Text style={styles.coinValueText}>${coin.current_price}</Text>
+        <Text
+          style={[
+            styles.pricePercentage,
+            coin.price_change_percentage_24h > 0
+              ? styles.priceUp
+              : styles.priceDown,
+          ]}
+        >
+          {coin.price_change_percentage_24h}%
+        </Text>
+      </View>
     </View>
   );
 }
@@ -23,12 +38,33 @@ const styles = StyleSheet.create({
   coinText: {
     color: "#ffff",
   },
+  coinValueText: {
+    color: "#ffff",
+    textAlign: "right",
+  },
   coinImage: {
     height: 30,
     width: 30,
   },
   coinName: {
     flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  textNameSymbolContainer: {
+    marginLeft: 10,
+  },
+  symbolText: {
+    color: "#434343",
+    textTransform: "uppercase",
+  },
+  pricePercentage: {
+    textAlign: "right",
+  },
+  priceUp: {
+    color: "green",
+  },
+  priceDown: {
+    color: "red",
   },
 });
 
